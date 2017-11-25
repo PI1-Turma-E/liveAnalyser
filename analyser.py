f44176aa60e7de1c5c57d50ca6c418414c7bed68
Victor Moura <victor_cmoura@hotmail.com>
2017-11-25 00:04:07 -0200

    Creating graph

--- analyser.py
+++ analyser.py
@@ -1,5 +1,7 @@
 import socket
 import datetime
+import numpy as np
+import matplotlib.pyplot as plt
 
 sniffed_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
@@ -8,22 +10,33 @@ data_size.append(0)
 operator = 0
 
 current_second = datetime.datetime.now().second
+
 while True:
-    packet = sniffed_socket.recvfrom(8080)[0]
+    try:
+        packet = sniffed_socket.recvfrom(8081)[0]
 
-    result_file = open('analysis_result.txt', 'w')
-    
-    if datetime.datetime.now().second == current_second:
-        data_size[operator] += len(packet)
-    else:
-        current_second = datetime.datetime.now().second    
-        operator += 1
-        data_size.append(0)
-    
-    for each in data_size:
-        result_file.write(str(each) + 'bytes' + '\n')
+        if datetime.datetime.now().second == current_second:
+            data_size[operator] += len(packet)
+        else:
+            current_second = datetime.datetime.now().second
+            operator += 1
+            data_size.append(0)
 
-    result_file.write('\nAverage: ' + str(sum(data_size)/len(data_size)) + 'bytes/sec\n')
+    except KeyboardInterrupt:
+        break
 
-    result_file.close()
-    
+result_file = open('analysis_result.txt', 'w')
+
+for index, each in enumerate(data_size):
+    result_file.write(str(index) + ': ' + str(each) + ' bytes' + '\n')
+
+result_file.write('\nAverage: ' + str(sum(data_size)/len(data_size)) + 'bytes/sec\n')
+
+result_file.close()
+
+X = np.linspace(1, len(data_size), len(data_size))
+
+plt.plot(X, data_size)
+plt.savefig('analysis_result.png')
+
+print("\nAnalysis finished!\n")
