a50c7f73cbffef5b1e38428776681528a2073e4a
Victor Moura <victor_cmoura@hotmail.com>
2017-11-25 00:30:56 -0200

    Added simultaneous analysis for Image and Control - Image data and control data are captured simultaneously - Image data and control data are saved in different text files - Graph shows both data in order to compare the usage

--- analyser.py
+++ analyser.py
@@ -5,43 +5,61 @@ import matplotlib.pyplot as plt
 
 sniffed_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-data_size = []
-data_size.append(0)
+image_data = [0]
+control_data = [0]
+
 operator = 0
 
 current_second = datetime.datetime.now().second
 
 while True:
     try:
-        packet = sniffed_socket.recvfrom(8081)[0]
+        image_packet = sniffed_socket.recvfrom(8081)[0]
+        control_packet = sniffed_socket.recvfrom(8080)[0]
 
         if datetime.datetime.now().second == current_second:
-            data_size[operator] += len(packet)
+            image_data[operator] += len(image_packet)
+            control_data[operator] += len(control_data)
         else:
             current_second = datetime.datetime.now().second
             operator += 1
-            data_size.append(0)
+            image_data.append(0)
+            control_data.append(0)
 
     except KeyboardInterrupt:
         break
 
-result_file = open('analysis_result.txt', 'w')
+image_result_file = open('image_data_result.txt', 'w')
+image_data = np.array(image_data)/1000
 
-data_size = np.array(data_size)/1000
+for index, each in enumerate(image_data):
+    image_result_file.write('Time: ' + str(index) + 's - Data: ' + str(each) + ' Kb' + '\n')
 
-for index, each in enumerate(data_size):
-    result_file.write('Time: ' + str(index) + 's - Data: ' + str(each) + ' Kb' + '\n')
+image_result_file.write('\nAverage: ' + str(sum(image_data)/len(image_data)) + ' kbps\n')
 
-result_file.write('\nAverage: ' + str(sum(data_size)/len(data_size)) + ' kbps\n')
+control_result_file = open('control_data_result.txt', 'w')
+control_data = np.array(control_data)/1000
 
-result_file.close()
+for index, each in enumerate(control_data):
+    control_result_file.write('Time: ' + str(index) + 's - Data: ' + str(each) + ' Kb' + '\n')
 
-X = np.linspace(1, len(data_size), len(data_size))
+control_result_file.write('\nAverage: ' + str(sum(control_data)/len(control_data)) + ' kbps\n')
 
-plt.plot(X, data_size)
+image_result_file.close()
+control_result_file.close()
+
+X = np.linspace(1, len(image_data), len(image_data))
+
+plt.plot(X, image_data, 'r', label='Image data')
+plt.plot(X, control_data, 'b', label='Control data')
+
 plt.ylabel('data quantity (kilobytes)')
 plt.xlabel('time elapsed (seconds)')
+
+plt.legend()
+
 plt.tight_layout()
+
 plt.savefig('analysis_result.png')
 
 print("\nAnalysis finished!\n")
